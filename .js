b8713b95d3e8424222d4f765cf4ce4aa3a608d5b
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:51:05 +0700

    Add human choice to lowercase

--- .js
+++ .js
@@ -1,7 +1,9 @@
-console.log("Welcome to the game!");
-
 const getComputerChoice = ()=> Math.floor(Math.random() * 3);
 const getHumanChoice = ()=> prompt("Please input your choice");
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+}
