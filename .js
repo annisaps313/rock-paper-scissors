b9db2b94f546dcbe50a42133a440565994d8ece6
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:37:58 +0700

    Remove playround function

--- .js
+++ .js
@@ -6,30 +6,6 @@ const getHumanChoice = ()=> prompt("Please input your choice");
 let humanScore = 0;
 let computerScore = 0;
 
-// function playRound(humanChoice, computerChoice){
-//     if (computerChoice === 0 && humanChoice === 1){
-//         humanChoice++;
-//         console.log("You win! Paper beats Rock");
-//     } else if (computerChoice === 0 && humanChoice === 2){
-//         computerChoice++;
-//         console.log("You Lose");
-//     } else if (computerChoice === 1 && humanChoice === 0){
-//         computerChoice++;
-//         console.log("You lose");
-//     } else if (computerChoice === 1 && humanChoice === 2){
-//         humanChoice++;
-//         console.log("You win");
-//     } else if (computerChoice === 2 && humanChoice === 0){
-//         humanChoice++;
-//         console.log("win");
-//     } else if (computerChoice === 2 && humanChoice === 1){
-//         computerChoice++;
-//         console.log("lose");
-//     } else{
-//         console.log("We are tied")
-//     }
-// }
-
 // const computerSelection = getComputerChoice();
 // const humanSelection = getHumanChoices;
 
