c8ca9c9b31c8249746875f663bc36b46b2d84043
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:36:31 +0700

    Add get human choice function

--- .js
+++ .js
@@ -1,6 +1,7 @@
 console.log("Welcome to the game!");
 
 const getComputerChoice = ()=> Math.floor(Math.random() * 3);
+const getHumanChoice = ()=> prompt("Please input your choice");
 
 let humanScore = 0;
 let computerScore = 0;
