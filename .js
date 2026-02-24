7af2389b65f07733f0fbf47b1c4594ea876f3519
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:38:19 +0700

    Remove play selection variable

--- .js
+++ .js
@@ -5,8 +5,3 @@ const getHumanChoice = ()=> prompt("Please input your choice");
 
 let humanScore = 0;
 let computerScore = 0;
-
-// const computerSelection = getComputerChoice();
-// const humanSelection = getHumanChoices;
-
-// playRound(humanSelection, computerSelection);
