91fbb3839eda30bd3d65f3bc0e64907b38baeee2
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:21:15 +0700

    Fix get human choice function

--- .js
+++ .js
@@ -1,36 +1,41 @@
 console.log("Welcome to the game!");
 
 let getComputerChoice = () => Math.floor(Math.random() * 3);
-let getHumanChoices = prompt("Please input your choice");
 
-let humanScore = 0;
-let computerScore = 0;
+let humanChoice;
+let getHumanChoices = ()=> {
+    humanChoice = prompt("Please input your choice");
+    return humanChoice;
+};
 
-function playRound(humanChoice, computerChoice){
-    if (computerChoice === 0 && humanChoice === 1){
-        humanChoice++;
-        console.log("You win! Paper beats Rock");
-    } else if (computerChoice === 0 && humanChoice === 2){
-        computerChoice++;
-        console.log("You Lose");
-    } else if (computerChoice === 1 && humanChoice === 0){
-        computerChoice++;
-        console.log("You lose");
-    } else if (computerChoice === 1 && humanChoice === 2){
-        humanChoice++;
-        console.log("You win");
-    } else if (computerChoice === 2 && humanChoice === 0){
-        humanChoice++;
-        console.log("win");
-    } else if (computerChoice === 2 && humanChoice === 1){
-        computerChoice++;
-        console.log("lose");
-    } else{
-        console.log("We are tied")
-    }
-}
+// let humanScore = 0;
+// let computerScore = 0;
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoices;
+// function playRound(humanChoice, computerChoice){
+//     if (computerChoice === 0 && humanChoice === 1){
+//         humanChoice++;
+//         console.log("You win! Paper beats Rock");
+//     } else if (computerChoice === 0 && humanChoice === 2){
+//         computerChoice++;
+//         console.log("You Lose");
+//     } else if (computerChoice === 1 && humanChoice === 0){
+//         computerChoice++;
+//         console.log("You lose");
+//     } else if (computerChoice === 1 && humanChoice === 2){
+//         humanChoice++;
+//         console.log("You win");
+//     } else if (computerChoice === 2 && humanChoice === 0){
+//         humanChoice++;
+//         console.log("win");
+//     } else if (computerChoice === 2 && humanChoice === 1){
+//         computerChoice++;
+//         console.log("lose");
+//     } else{
+//         console.log("We are tied")
+//     }
+// }
 
-playRound(humanSelection, computerSelection);
+// const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoices;
+
+// playRound(humanSelection, computerSelection);
