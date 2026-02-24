fac5693311544011d4c4a24c6bb29af3ab72ebe2
annisaps313 <annisaps313@gmail.com>
2026-02-24 13:52:53 +0700

    Add computer choice string

--- .js
+++ .js
@@ -6,4 +6,13 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
+
+    // convert random number computer choice
+    if (computerChoice == 0){
+        return "rock";
+    } else if (computerChoice == 1){
+        return "paper";
+    } else if (computerChoice == 2){
+        return "scissors";
+    }
 }
